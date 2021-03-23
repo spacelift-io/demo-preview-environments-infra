a25130a6f0937b5731ff9cc31d4b6d94dea9d7c0
Jakub Martin <jakub.wit.martin@gmail.com>
2021-03-23 15:07:08 +0100

    Set up domain properly.

--- main.tf
+++ main.tf
@@ -20,18 +20,23 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "aws" {
+  alias  = "us-east-1"
+  region = var.aws_region
+}
+
 resource "aws_lambda_function" "hello" {
-  s3_bucket = "spacelift-demo-preview-environments-service"
-  s3_key = "${var.code_version}.zip"
-  function_name = "hello${local.suffix}"
-  role = aws_iam_role.iam_for_lambda_tf.arn
-  handler = "index.handler"
+  s3_bucket        = "spacelift-demo-preview-environments-service"
+  s3_key           = "${var.code_version}.zip"
+  function_name    = "hello${local.suffix}"
+  role             = aws_iam_role.iam_for_lambda_tf.arn
+  handler          = "index.handler"
   source_code_hash = var.code_version
-  runtime = "nodejs12.x"
+  runtime          = "nodejs12.x"
 }
 
 resource "aws_iam_role" "iam_for_lambda_tf" {
-  name = "iam_for_lambda_tf${local.suffix}"
+  name               = "iam_for_lambda_tf${local.suffix}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -50,10 +55,10 @@ EOF
 }
 
 resource "aws_lambda_permission" "hello" {
-  statement_id = "AllowAPIGatewayInvoke"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.hello.arn
-  principal = "apigateway.amazonaws.com"
+  principal     = "apigateway.amazonaws.com"
 }
 
 resource "aws_api_gateway_rest_api" "hello" {
@@ -67,9 +72,9 @@ resource "aws_api_gateway_resource" "hello" {
 }
 
 resource "aws_api_gateway_method" "hello" {
-  rest_api_id = aws_api_gateway_rest_api.hello.id
-  resource_id = aws_api_gateway_resource.hello.id
-  http_method = "GET"
+  rest_api_id   = aws_api_gateway_rest_api.hello.id
+  resource_id   = aws_api_gateway_resource.hello.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -82,12 +87,12 @@ resource "aws_api_gateway_deployment" "hello_v1" {
 }
 
 resource "aws_api_gateway_integration" "hello" {
-  rest_api_id = aws_api_gateway_rest_api.hello.id
-  resource_id = aws_api_gateway_resource.hello.id
-  http_method = aws_api_gateway_method.hello.http_method
+  rest_api_id             = aws_api_gateway_rest_api.hello.id
+  resource_id             = aws_api_gateway_resource.hello.id
+  http_method             = aws_api_gateway_method.hello.http_method
   integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = aws_lambda_function.hello.invoke_arn
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello.invoke_arn
 }
 
 output "url" {
@@ -95,9 +100,44 @@ output "url" {
 }
 
 resource "aws_api_gateway_base_path_mapping" "webhooks" {
+  depends_on = [
+    aws_route53_record.endpoint-certificate
+  ]
+
   count = var.domain_name != "" ? 1 : 0
 
   api_id      = aws_api_gateway_rest_api.hello.id
   stage_name  = aws_api_gateway_deployment.hello_v1.stage_name
   domain_name = "${var.environment}.${var.domain_name}"
+}
+
+resource "aws_acm_certificate" "endpoint-certificate" {
+  provider = aws.us-east-1
+
+  domain_name       = "webhooks.${var.domain_name}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "endpoint-certificate" {
+  provider = aws.us-east-1
+
+  for_each = {
+    for dvo in aws_acm_certificate.endpoint-certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records = [
+  each.value.record]
+  ttl     = 60
+  type    = each.value.type
+  zone_id = var.zone_id
 }
