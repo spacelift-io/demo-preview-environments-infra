656aacaeaa7af2fbabddfc2cc42770f714cd55b0
Jacob Martin <kubam@spacelift.io>
2021-03-25 16:11:37 +0100

    Change the URL

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_api_gateway_rest_api" "hello" {
 resource "aws_api_gateway_resource" "hello" {
   rest_api_id = aws_api_gateway_rest_api.hello.id
   parent_id   = aws_api_gateway_rest_api.hello.root_resource_id
-  path_part   = "hello"
+  path_part   = "greetings"
 }
 
 resource "aws_api_gateway_method" "hello" {
